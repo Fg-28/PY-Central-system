34a53bc96be88413a8e303b043d5ae38612a9872
Fg-28 <fenilgabani5851@gmail.com>
2025-07-04 14:16:15 +0530

    Update GST_Recon_Venus.py

--- scripts/GST_Recon_Venus.py
+++ scripts/GST_Recon_Venus.py
@@ -7,14 +7,16 @@ root = tk.Tk()
 root.withdraw()
 
 # First message
-messagebox.showerror("Error", "Scheduled System Maintenance in Progress.\nSome features may be temporarily unavailable.")
+messagebox.showerror("Error", "Scheduled System Maintenance in Progress.")
 
 # Wait 5 seconds
 time.sleep(1)
 
 # Second message
-messagebox.showerror("Error", "Estimated Downtime: 06:30 TM-india, kolkata.\nWe appreciate your patience.")
+messagebox.showerror("Error", "Estimated Downtime: 08:30 TM-india, kolkata.\nWe appreciate your patience.")
 
+time.sleep(1)
+messagebox.showerror("Error", "We will get back to you when server is ready.")
 
 
 # Optional: Exit
